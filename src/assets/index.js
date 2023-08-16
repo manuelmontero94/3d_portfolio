3ee0c54b13e373d0955e51319d32a82d3f5426d7
Manuel Montero <mmonteroc@qubosystems.com>
2023-08-15 19:04:54 -0600

    Poryecto casi finalizado

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,43 +1,46 @@
-import logo from './logo.svg';
 import backend from './backend.png';
+import close from './close.svg';
 import creator from './creator.png';
+import github from './github.png';
+import logo from './logo.svg';
+import menu from './menu.svg';
 import mobile from './mobile.png';
 import web from './web.png';
-import github from './github.png';
-import menu from './menu.svg';
-import close from './close.svg';
 
+import cifradocesar from './cifradocesar.png';
+import favoritesapp from './favoritesapp.png';
+import portfolio from './portfolio.png';
+import portfolio2 from './portfolio2.png';
 import css from './tech/css.png';
 import docker from './tech/docker.png';
 import figma from './tech/figma.png';
+import firebase from './tech/firebase.png';
 import git from './tech/git.png';
 import html from './tech/html.png';
 import javascript from './tech/javascript.png';
 import mongodb from './tech/mongodb.png';
+import mysql from './tech/mysql.png';
+import nestjs from './tech/nestjs.png';
 import nodejs from './tech/nodejs.png';
 import reactjs from './tech/reactjs.png';
 import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import threejs from './tech/threejs.svg';
 import sqlserver from './tech/sqlserver.png';
-import mysql from './tech/mysql.png';
-import nestjs from './tech/nestjs.png';
-import firebase from './tech/firebase.png';
-import cifradocesar from './cifradocesar.png';
-import portfolio from './portfolio.png';
-import favoritesapp from './favoritesapp.png';
-import portfolio2 from './portfolio2.png';
+import tailwind from './tech/tailwind.png';
+import threejs from './tech/threejs.svg';
+import typescript from './tech/typescript.png';
 
+import logoAbbott from './company/logoAbbott.png';
 import meta from './company/meta.png';
 import shopify from './company/shopify.png';
 import starbucks from './company/starbucks.png';
 import tesla from './company/tesla.png';
+import logoqubo from './logoqubo.jpg';
+import abbott from './abbott.png';
 
 import carrent from './carrent.png';
 import jobit from './jobit.png';
-import tripguide from './tripguide.png';
 import traveladvisor from './traveladvisor.png';
+import tripguide from './tripguide.png';
 
 export {
 	logo,
@@ -77,4 +80,7 @@ export {
 	portfolio,
 	favoritesapp,
 	portfolio2,
+	abbott,
+	logoqubo,
+	logoAbbott,
 };
